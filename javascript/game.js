4a81172f64ab8034925b6e649b04b34ed00a0064
lethalSid <senien.hicks@gmail.com>
2019-09-02 17:04:57 -0400

    erased console.logs for cleaner appearance

--- javascript/game.js
+++ javascript/game.js
@@ -1,5 +1,3 @@
-console.log("hooked up");
-
 $(document).ready(function () {
 
     //scoreboard
@@ -15,7 +13,6 @@ $(document).ready(function () {
 
     //random numbers generated for buttons and computer score
     var compGuess = Math.floor(Math.random() * (120 - 34) + 34);
-    console.log(compGuess);
 
     function resetScores() {
         userScore = 0;
@@ -23,45 +20,32 @@ $(document).ready(function () {
         $("#your-score").text(userScore);
     }
 
-
     function buttonLogic() {
 
         $("#comp-score").text(compGuess);
         $("#status-text").text("Now Playing");
 
         $("#btn-1").click(function () {
-            console.log("You clicked a button1");
             userScore += btn1;
-            console.log(userScore);
             $("#your-score").text(userScore);
-            console.log(btn1);
             gameLogic();
         });
 
         $("#btn-2").click(function () {
             userScore += btn2
-            console.log("You clicked a button2");
-            console.log(userScore);
             $("#your-score").text(userScore);
-            console.log(btn2);
             gameLogic();
         });
 
         $("#btn-3").click(function () {
             userScore += btn3
-            console.log("You clicked a button3");
-            console.log(userScore);
             $("#your-score").text(userScore);
-            console.log(btn3);
             gameLogic();
         });
 
         $("#btn-4").click(function () {
             userScore += btn4
-            console.log("You clicked a button4");
-            console.log(userScore);
             $("#your-score").text(userScore);
-            console.log(btn4);
             gameLogic();
         });
     }
