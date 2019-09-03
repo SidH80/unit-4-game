5c0e3304f1d0111aac5c451ded0025a4e01652b5
lethalSid <senien.hicks@gmail.com>
2019-09-02 20:20:41 -0400

    game working! yayeah!

--- javascript/clipboard.js
+++ javascript/clipboard.js
@@ -1,63 +1,60 @@
 $(document).ready(function () {
 
-    var userScore;
-    var compGuess;
-
+    //scoreboard
+    var userScore = 0;
     var wins = 0;
     var losses = 0;
 
-    var btn1;
-    var btn2;
-    var btn3;
-    var btn4;
+    //button variables
 
-    function initGame() {
 
+    var btn1 = Math.floor(Math.random() * 12) + 1;
+    var btn2 = Math.floor(Math.random() * 12) + 1;
+    var btn3 = Math.floor(Math.random() * 12) + 1;
+    var btn4 = Math.floor(Math.random() * 12) + 1;
+
+    //random numbers generated for buttons and computer score
+    var compGuess = Math.floor(Math.random() * (120 - 34) + 34);
+
+
+    function resetScores() {
         userScore = 0;
-
-        compGuess = Math.floor(Math.random() * (120 - 34) + 34);
-
-        btn1 = Math.floor(Math.random() * 12) + 1;
-        btn2 = Math.floor(Math.random() * 12) + 1;
-        btn3 = Math.floor(Math.random() * 12) + 1;
-        btn4 = Math.floor(Math.random() * 12) + 1;
-
-        $("#comp-score").text(compGuess);
+        $("#status-text").text("Now Playing");
         $("#your-score").text(userScore);
+        $("#comp-score").text(compGuess);
     }
 
-    initGame();
+    resetScores();
 
-    function isPlaying() {
+    function initializeGame() {
+
+        $("#status-text").text("Now Playing");
+
         $("#btn-1").click(function () {
             userScore += btn1;
             $("#your-score").text(userScore);
             gameLogic();
-        })
+        });
 
         $("#btn-2").click(function () {
-            userScore += btn2;
+            userScore += btn2
             $("#your-score").text(userScore);
             gameLogic();
-        })
+        });
 
         $("#btn-3").click(function () {
-            userScore += btn3;
+            userScore += btn3
             $("#your-score").text(userScore);
             gameLogic();
-
-        })
+        });
 
         $("#btn-4").click(function () {
-            userScore += btn4;
+            userScore += btn4
             $("#your-score").text(userScore);
             gameLogic();
-
         });
 
     }
-
-    isPlaying();
 
     function gameLogic() {
 
@@ -66,17 +63,17 @@ $(document).ready(function () {
             wins++;
             $("#wins-text").text(wins);
             $("#status-text").text("You've Won!");
-            initGame();
+            alert("You've won!")
         }
         else if(userScore > compGuess)
         {
-            losses++;
             $("#status-text").text("Game Over.");
             $("#losses-text").text(losses);
-            initGame();
+            losses++;
         }
+
     }
 
-
+    initializeGame();
 
 });
